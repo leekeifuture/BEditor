f1b65bb0cbaa95bad726eb8eab62a39fd0e9f44b
leekeifuture <opnoe09@gmail.com>
2020-01-07 17:02:05 +0300

    Initial commit

---
+++ src/main/java/com/example/BEditor/BEditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.BEditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BEditor/BEditorApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.BEditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
