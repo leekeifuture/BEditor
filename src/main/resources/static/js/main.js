767994701a84e52de528c9a8d286326f0bbe3fdd
leekeifuture <opnoe09@gmail.com>
2020-02-02 14:59:50 +0300

    Setted Vuex and added centralized storage

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import '@babel/polyfill'
 import 'api/resource'
 import App from 'pages/App.vue'
+import store from 'store/store'
 import {connect} from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -13,6 +15,7 @@ Vue.use(Vuetify)
 
 new Vue({
     el: '#app',
+    store, // means -> store: store
     render: a => a(App),
     // vuetify: new Vuetify({}) // use for 2 version of vuetify or higher
 })
--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -13,13 +13,14 @@
                 Need to sign in via <a href="/login">Google</a>
             </v-container>
             <v-container v-if="profile">
-                <messages-list :messages="messages" />
+                <messages-list />
             </v-container>
         </v-content>
     </v-app>
 </template>
 
 <script>
+    import {mapMutations, mapState} from 'vuex'
     import MessagesList from 'components/messages/MessageList.vue'
     import {addHandler} from 'util/ws'
 
@@ -27,29 +28,25 @@
         components: {
             MessagesList
         },
-        data() {
-            return {
-                messages: frontendData.messages,
-                profile: frontendData.profile
-            }
-        },
+        computed: mapState(['profile']),
+        methods: mapMutations([
+            'addMessageMutation',
+            'updateMessageMutation',
+            'removeMessageMutation'
+        ]),
         created() {
             addHandler(data => {
                 if (data.objectType === 'MESSAGE') {
 
-                    const index = this.messages.findIndex(item => item.id === data.body.id)
-
                     switch (data.eventType) {
                         case 'CREATE':
+                            this.addMessageMutation(data.body)
+                            break
                         case 'UPDATE':
-                            if (index > -1) {
-                                this.messages.splice(index, 1, data.body)
-                            } else {
-                                this.messages.push(data.body)
-                            }
+                            this.updateMessageMutation(data.body)
                             break
                         case 'REMOVE':
-                            this.messages.splice(index, 1)
+                            this.removeMessageMutation(data.body)
                             break
                         default:
                             console.error(`Looks like the event type if unknown "${data.eventType}"`)
